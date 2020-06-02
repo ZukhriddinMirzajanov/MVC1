4a3d4415ce6c3d592ec66b1163ed9c8e22c6485c
Zukhriddin <lookslike_98@outlook.com>
2020-06-02 20:30:12 +0200

    Updated

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
-// Connecting to Database
+// Connection to Database
 mongoose
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to the database..."))
+  .then(() => console.log("Connected to database..."))
   .catch((err) => console.log("Error during the Connection", err));
 
 app.use("/", routerHome);
