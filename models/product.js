515dc678e3d06de31bd1b1df5ff0ba5d7ccd4384
Zukhriddin <lookslike_98@outlook.com>
2020-06-20 22:42:10 +0200

    C

--- models/product.js
+++ models/product.js
@@ -9,10 +9,10 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
-  image: {
-    type: String,
-    required: false,
-  },
+  // image: {
+  //   type: File,
+  //   required: true,
+  // },
 });
 
 module.exports = mongoose.model("Product", productSchema);
--- routes/home.js
+++ routes/home.js
@@ -3,47 +3,49 @@ const router = express.Router();
 const Product = require("../models/product");
 
 // Getting all products
-router.get("/", async (req, res) => {
-  let searchOptions = {};
-  if (req.query.name != null && req.query.name !== "") {
-    searchOptions.name = new RegExp(req.query.name, "i");
-  }
-  try {
-    const products = await Product.find(searchOptions);
-    res.render("home", {
-      products: products,
-      searchOptions: req.query,
-    });
-  } catch {
-    res.redirect("/");
-  }
+router.get("/", async(req, res) => {
+
 });
 
 router.get("/about", (req, res) => {
-  res.render("about/about");
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, "i");
+    }
+    try {
+        const products = await Product.find(searchOptions);
+        res.render("about", {
+            products: products,
+            searchOptions: req.query,
+        });
+    } catch {
+        res.redirect("/");
+    }
 });
+
 // Getting new product
 router.get("/product", (req, res) => {
-  res.render("products/product", { product: new Product() });
+    res.render("products/product", { product: new Product() });
 });
 
 // Creating new product
-router.post("/", async (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    cost: req.body.cost,
-  });
-
-  try {
-    const newProduct = await product.save();
-    // res.redirect(`/${newProduct.id}`);
-    res.redirect("/");
-  } catch {
-    res.render("products/product", {
-      product: product,
-      errorMessage: "Error during the creating new Product",
+router.post("/", async(req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        cost: req.body.cost,
+        // imgage: req.body.imgage,
     });
-  }
+
+    try {
+        const newProduct = await product.save();
+        // res.redirect(`/${newProduct.id}`);
+        res.redirect("/");
+    } catch {
+        res.render("products/product", {
+            product: product,
+            errorMessage: "Error during the creating new Product",
+        });
+    }
 });
 
 module.exports = router;
